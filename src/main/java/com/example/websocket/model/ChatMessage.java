18a350df20323158f20d4e00674caf58cc9dea32
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-03-12 15:02:08 +0400

    Initial commit

---
+++ src/main/java/com/example/websocket/model/ChatMessage.java
@@ -0,0 +1,46 @@
+package com.example.websocket.model;
+
+import java.time.LocalDateTime;
+
+public class ChatMessage {
+
+    private String sender;
+    private String content;
+    private String type;
+    private LocalDateTime timestamp;
+
+    public ChatMessage() {
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+}
